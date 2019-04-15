9be5e9595c9c1890f0b617ac0a80ceb9360a4037
souha07 <extend023-safezone@yahoo.co.jp>
2019-04-15 20:28:17 +0900

    main.rbから文字列・生成数・出力ファイル名を指定できるようにコマンドライン引数追加

---
+++ Sample.bat
@@ -0,0 +1,5 @@
+main.rb "%%10s" 2 "./result_filename.txt"
+
+main.rb "TEL:090-%%4d-%%4d" 3
+
+pause
--- main.rb
+++ main.rb
@@ -7,9 +7,36 @@ require './RandomString'
 ########################################
 # main
 ########################################
-rs = RandomString.new()
-outList = rs.Create( "あいうえお-%4d", 10 )
 
-puts outList
+# 引数チェック
+if ARGV.size < 2
+    puts "Argument Error"
+    puts "  ARGV[0] = format string"
+    puts "  ARGV[1] = createNum"
+    puts "  ARGV[2] = result filename"
+
+    exit
+end
+format = ARGV[0]
+createNum = ARGV[1].to_i
+
+# 結果ファイル名があれば設定、なければresult.txt
+result = "./result.txt"
+result = ARGV[2].dup if ARGV[2] != nil 
+
+
+# ランダム文字列生成
+rs = RandomString.new()
+outList = rs.Create( format, createNum )
+
+outList.each { |item| puts item.encode( "UTF-8" ) } 
+puts ""
+
+# 結果書き込み
+File::open( result, "w" ) do |wf|
+    outList.each do |item|
+        wf.puts( item )
+    end
+end
 
 puts "complete"
